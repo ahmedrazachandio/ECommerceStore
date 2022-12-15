2d4575afbef9a76b02498b931c494afac7ae2f81
Ali Raza <107404832+aliraza-solcoders@users.noreply.github.com>
2022-12-16 00:05:52 +0500

    code push

--- MernStackCrud/src/components/Register.jsx
+++ MernStackCrud/src/components/Register.jsx
@@ -1,13 +1,152 @@
-import React from "react";
+// import logo from './logo.svg';
+// import './App.css';
+import { useFormik } from 'formik';
 import { NavLink } from "react-router-dom";
+import * as yup from 'yup';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
-const Register = () => {
-    const setData = (e) =>{
-        console.log(e.target.value)
-        
+function AddProduct() {
+
+  const [products, setProducts] = useState([])
+  const [loadProduct, setLoadProduct] = useState(false)
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [editingProduct, setEditingProduct] = useState(null)
+
+
+  const getAllProducts = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5001/products`)
+      console.log("response: ", response.data);
+
+      setProducts(response.data.data)
+
+    } catch (error) {
+      console.log("error in getting all products", error);
     }
+  }
+
+  const deleteProduct = async (id) => {
+    try {
+      const response = await axios.delete(`http://localhost:5001/product/${id}`)
+      console.log("response: ", response.data);
+
+      setLoadProduct(!loadProduct)
+
+    } catch (error) {
+      console.log("error in getting all products", error);
+    }
+  }
+
+  const editMode = (product) => {
+    setIsEditMode(!isEditMode)
+    setEditingProduct(product)
+
+    editFormik.setFieldValue("productName", product.name)
+    editFormik.setFieldValue("productPrice", product.price)
+    editFormik.setFieldValue("productDescription", product.description)
+   
+  }
+
+  useEffect(() => {
+
+    getAllProducts()
+
+  }, [loadProduct])
+
+
+  const myFormik = useFormik({
+    initialValues: {
+      productName: '',
+      productPrice: '',
+      productDescription: '',
+    },
+    validationSchema:
+      yup.object({
+        productName: yup
+          .string('Enter your product name')
+          .required('product name is required')
+          .min(3, "please enter more then 3 characters ")
+          .max(20, "please enter within 20 characters "),
+
+        productPrice: yup
+          .number('Enter your product price')
+          .positive("enter positive product price")
+          .required('product name is required'),
+
+        productDescription: yup
+          .string('Enter your product Description')
+          .required('product name is required')
+          .min(3, "please enter more then 3 characters ")
+          .max(500, "please enter within 20 characters "),
+      }),
+    onSubmit: (values) => {
+      console.log("values: ", values);
+
+      axios.post(`http://localhost:5001/product`, {
+        name: values.productName,
+        price: values.productPrice,
+        description: values.productDescription,
+      })
+        .then(response => {
+          console.log("response: ", response.data);
+          setLoadProduct(!loadProduct)
+
+        })
+        .catch(err => {
+          console.log("error: ", err);
+        })
+    },
+  });
+  const editFormik = useFormik({
+    initialValues: {
+      productName: '',
+      productPrice: '',
+      productDescription: '',
+    },
+    validationSchema:
+      yup.object({
+        productName: yup
+          .string('Enter your product name')
+          .required('product name is required')
+          .min(3, "please enter more then 3 characters ")
+          .max(20, "please enter within 20 characters "),
+
+        productPrice: yup
+          .number('Enter your product price')
+          .positive("enter positive product price")
+          .required('product name is required'),
+
+        productDescription: yup
+          .string('Enter your product Description')
+          .required('product name is required')
+          .min(3, "please enter more then 3 characters ")
+          .max(500, "please enter within 20 characters "),
+      }),
+    onSubmit: (values) => {
+      console.log("values: ", values);
+
+      axios.put(`http://localhost:5001/product/${editingProduct.id}`, {
+        name: values.productName,
+        price: values.productPrice,
+        description: values.productDescription,
+      })
+        .then(response => {
+          console.log("response: ", response.data);
+          setLoadProduct(!loadProduct)
+
+        })
+        .catch(err => {
+          console.log("error: ", err);
+        })
+    },
+  });
+
   return (
     <div className="container mt-3">
+      Register
+      
+{/* 
       <NavLink to="/">Home</NavLink>
       <div className="row mt-3">
         <div className="mb-3 col-lg-6 col-md-12 col-12">
@@ -20,7 +159,7 @@ const Register = () => {
             className="form-control"
             id="exampleFormControlInput1"
             placeholder="Enter Your Name"
-            onChange={setData}
+            // onChange={setData}
             // value={}
           />
         </div>
@@ -34,7 +173,7 @@ const Register = () => {
             className="form-control"
             id="exampleFormControlInput1"
             placeholder="ahmadraza.azt@gmail.com"
-            onChange={setData}
+            // onChange={setData}
             // value={}
           />
         </div>
@@ -55,9 +194,9 @@ const Register = () => {
           className="btn btn-primary col-lg-12 col-md-12 col-12">
           Submit
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
 
-export default Register;
+export default AddProduct;
